4194f6231c5eb5ed3472eb7da186032e597e325d
Javier Pons <jpescandell@hotmail.com>
2018-02-13 11:31:08 +0200

    done

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function reducer_books() {
-   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
-   ];
-}
